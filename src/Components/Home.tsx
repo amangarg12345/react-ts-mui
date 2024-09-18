2e7305e835fd84ea4d27288a29855f8b19c91cee
Aman Garg <amangarg24071995@gmail.com>
2024-09-18 18:58:54 +0530

    Design of Home Page

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,10 +1,25 @@
+import { Box, Button, InputBase } from '@mui/material'
 import React from 'react'
+import styled from 'styled-components'
 
+const Val = styled(InputBase)`
+background-color: lightblue;
+border-style: solid;
+margin: 20px 10px 20px 10px;
+padding : 5px 0 5px 10px;
+width : 150px;
+`
 const Home = () => {
   return (
-    <div>
-      <h1>HOme</h1>
-    </div>
+    <>
+    <Box>
+      <Val value="Count" readOnly size='small'></Val>
+      <Val value="20" readOnly size='small'></Val>
+      <Button variant="outlined">Increase</Button>
+      <Button variant="outlined">Decrease</Button>
+    </Box>
+    </>
+    
   )
 }
 
